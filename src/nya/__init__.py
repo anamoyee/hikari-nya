a89b8f01013e22c633c65281d3a70016bc83e225
anamoyee <thecreatorrr333@gmail.com>
2025-05-05 22:32:48 +0200

    first version, for now contains just make_loopback_link()

---
+++ src/nya/__init__.py
@@ -0,0 +1,13 @@
+"""# Useful stuff for hikari-based discord bot projects.
+
+```
+>>> from tcrutils.print import gay
+>>> print(gay('nyaaaaa')) # uwu :3
+```
+"""
+
+from ._version import __version__
+
+__tcr_rainbow__ = True
+
+from .url import make_loopback_link
